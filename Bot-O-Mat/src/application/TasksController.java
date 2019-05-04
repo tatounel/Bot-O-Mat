61ac85531d36e7ddedc63c6e5c79c550d378aa96
Tania Nelzy <tania@Tanias-Air.home>
2019-05-04 02:00:53 -0400

    Final

--- Bot-O-Mat/src/application/TasksController.java
+++ Bot-O-Mat/src/application/TasksController.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package application;
 
@@ -33,7 +33,7 @@ import javafx.util.Duration;
  *
  */
 public class TasksController implements Initializable{
-	
+
 // configure the table
 @FXML
 private TableView<Task> tableView;
@@ -63,7 +63,7 @@ private String s = "";
 private ObservableList<Task> data = FXCollections.observableArrayList();
 private ObservableList<Task> randomSelection = FXCollections.observableArrayList();
 
-	
+	//creating list of tasks to populate tableView
 	public ObservableList<Task> getData(){
 		data.add(new Task("do the dishes", "1000"));
 		data.add(new Task("sweep the house", "3000"));
@@ -75,9 +75,10 @@ private ObservableList<Task> randomSelection = FXCollections.observableArrayList
 		data.add(new Task("give the dog a bath", "14500"));
 		data.add(new Task("bake some cookies", "8000"));
 		data.add(new Task("wash the car", "20000"));
-		
+
 		Random rand = new Random();
 
+		//randomly assign five tasks
 		for(int i=0; i<5; i++) {
 			Task temp = data.get(rand.nextInt(data.size()));
 			if (!randomSelection.contains(temp)) {
@@ -87,22 +88,22 @@ private ObservableList<Task> randomSelection = FXCollections.observableArrayList
 				i-=1;
 		}
 
-		return randomSelection; 
+		return randomSelection;
 	}
 
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+
 		// set up the columns in the table
-		
+
 		taskDescriptionColumn.setCellValueFactory(new PropertyValueFactory<Task, String>("description"));
 		etaColumn.setCellValueFactory(new PropertyValueFactory<Task, String>("eta"));
 		checkBoxColumn.setCellValueFactory(new PropertyValueFactory<Task, String>("select"));
 		tableView.setItems(getData());
 		etaColumn.setVisible(false);
-		//continuously runs timelabel() 
+		//continuously runs timelabel()
 		animation = new Timeline(new KeyFrame(Duration.seconds(1), e -> timelabel()));
 		animation.setCycleCount(Timeline.INDEFINITE);
 		animation.play();
@@ -121,55 +122,55 @@ private ObservableList<Task> randomSelection = FXCollections.observableArrayList
 
 			}
 		}
-	}    
-	
+	}
+
 	//counts down eta of task with help of animation in timelabel()
 	public void timeCounter() {
 		s = "Time left: "+ tmp + " secs";
-		
+
 		if(tmp > 0 ) {
 			tmp --;
-		} 
+		}
 		if (tmp == 0) {
 			s = "Completed task!";
 		}
-		
+
 		timerLabel.setText(s);
 	}
-	
+
 	@FXML
 	//deletes selected checkBoxes (completed tasks) when delete row button is clicked
 	public void deleteSelectedRows(ActionEvent event) {
 		ObservableList<Task> rowsToRemove = FXCollections.observableArrayList();
-		
+
 		for(Task task: randomSelection) {
 			if(task.getSelect().isSelected())
 				rowsToRemove.add(task);
 		}
 		randomSelection.removeAll(rowsToRemove);
 	}
-	
+
 	/**
 	 * When this method is called, it will change the Scene to the Main scene
 	 */
 	public void changeScreenButtonPushed(ActionEvent event) throws IOException {
 
 		try {
-			
+
 			Parent root = FXMLLoader.load(getClass().getResource("/FXMLDocument.fxml"));
 
 			Scene scene = new Scene(root);
-			
+
 			// This line gets the Stage information
 			Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
 			window.setScene(scene);
-			
+
 			window.show();
-			
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
